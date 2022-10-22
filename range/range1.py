314567f799fdd17ec34000a1d900a69c329f027b
zxczxczc151533 <munkuy6974@gmail.com>
2022-10-22 14:14:18 +0900

    range

---
+++ range/range1.py
@@ -0,0 +1,16 @@
+temp = range(10)
+# print(temp)
+temp = range(5,10)
+# print(temp)
+temp = list(range(3, 30, 3))
+# print(temp)
+
+temp = list(range(0,10))
+
+# for item in temp:
+    # print("g")
+
+# temp = [item for item in range(0, 10)]
+temp = ["안녕" for item in range(0, 10)]
+
+print(temp)
