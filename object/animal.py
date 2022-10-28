08056a5f504f93ec09fb77c310d391b6ae889713
zxczxczc151533 <munkuy6974@gmail.com>
2022-10-28 14:17:45 +0900

    object

--- object/animal.py
+++ object/animal.py
@@ -1,11 +1,34 @@
-
 class Animal:
-    def __init__(self, age) -> None:
-        self,age = age
-    
-    def eaf(self):
+
+    count = 0
+
+    def __init__(self,
+                 age: int = 0
+                 ):
+        """
+
+        Args:
+            age (int, optional): 동물의 나이. 기본값은 0.
+        """
+
+        self.age = age
+
+        self.name = f"{Animal.count}번"
+
+        Animal.count = Animal.count + 1
+
+        return
+
+    def eat(self):
         print(f"동물 {self.name}이 음식을 먹었습니다.")
-        
+
     def get_age(self):
         print(f"동물 '{self.name}'의 나이는 {self.age}살 입니다.")
-            
+
+    @classmethod
+    def get_count(cls):
+        print(f"동물이 {cls.count}마리 생성되었습니다.")
+
+    @staticmethod
+    def hello():
+        print(f"동물 농장에 오신 것을 환영합니다.")
--- object/objest_test.py
+++ object/objest_test.py
@@ -1,8 +1,38 @@
 from animal import Animal
+from cat import Cat
+from dog import Dog
 
-new_animal = Animal(0)
+Animal.hello()
 
-new_animal.eat()
+print()
 
-new_animal.get_age()
+a = Animal(12)
+b = Cat("나옹", 5)
+c = Dog("멍", 3)
 
+a.eat()
+b.eat()
+
+print()
+
+a.get_age()
+b.get_age()
+c.get_age()
+
+
+print()
+
+Animal.get_count()
+Cat.get_count()
+Dog.get_count()
+
+# 동물 농장에 오신 것을 환영합니다.
+
+# 동물 0번이 음식을 먹었습니다.     
+# 고양이 '나옹'이 음식을 먹었습니다.
+
+# 동물 '0번'의 나이는 12살 입니다.
+# 동물 '나옹'의 나이는 5살 입니다.
+
+# 동물이 2마리 생성되었습니다.
+# 고양이가 1마리 생성되었습니다.
